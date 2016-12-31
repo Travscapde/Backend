cfdc7bc8b28ebbee13227b677ab2e9a0ac487e83
Hassan <hassan.ali.askari@gmail.com>
2016-12-31 22:18:24 +0800

    Added location_info schema

---
+++ models/location_info.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var locationInfoSchema = new Schema({
+    name: { type: String, required: true, unique: true },
+    summary : String
+});
+
+
+// on every save, add the date
+locationInfoSchema.pre('save', function (next) {
+    var currentDate = new Date();
+    this.updated_at = currentDate;
+    if (!this.created_at)
+        this.created_at = currentDate;
+
+    next();
+});
+
+
+var LocationInfoSchema = mongoose.model('LocationInfoSchema', locationInfoSchema);
+module.exports = LocationInfoSchema;
+
+
